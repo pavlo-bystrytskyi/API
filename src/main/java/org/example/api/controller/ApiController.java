48d8d758dc330c19af8b7b92ea50d029dad0b46b
mariyamango <de.msomkova@gmail.com>
2024-09-25 10:13:07 +0200

    Added getById endpoint

--- src/main/java/org/example/api/controller/ApiController.java
+++ src/main/java/org/example/api/controller/ApiController.java
@@ -1,8 +1,13 @@
 package org.example.api.controller;
 
+//Write an endpoint that returns a Rick&Morty character. When you enter GET http://localhost:8080/api/characters/2 in Postman,
+// the character with ID 2 should be returned.
+//``{"id": 2, "name": "Rick Sanchez", "species": "Human"}
+
 import org.example.api.dto.RickMortyApiResponse;
 import org.example.api.model.Character;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
@@ -22,5 +27,16 @@ public class ApiController {
         }
 
         return response.results();
+    }
+
+    @GetMapping("/{id}")
+    public Character getCharacterById(@PathVariable String id) {
+        RestClient restClient = RestClient.builder().baseUrl("https://rickandmortyapi.com/api").build();
+        Character response = restClient.get().uri("/character/{id}", id).retrieve().body(Character.class);
+        if (response == null) {
+            throw new RuntimeException("Empty response");
+        }
+
+        return response;
     }
 }
